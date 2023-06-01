a9bcbeffadf15aa49ab99799b405b7060a6c432a
irenepintor <pintor.irene@outlook.com>
2023-06-01 13:00:28 -0300

    Navbar modified | Banners added | Styling index.html

---
+++ README.md
@@ -0,0 +1,17 @@
+E-commerce Cotillón y Repostería San Pedro
+
+Build with:
+    HTML5
+    CSS | SASS
+    Javascript
+
+
+
+Commits:
+
+"Stating layouts, scripts and styles | Static image folder, replaced later on."
+
+_Created static files with HTML5
+_Static image file | Will be replaced later on
+_Set script files for product card creation with Javascript
+_Added other script file for global website dynamics
---
+++ assets/js/global-scripts.js
@@ -0,0 +1,26 @@
+//Simulador de pago en cuotas y recarga impositiva
+// let tipoConsumidor = (prompt("¿Eres Consumidor Final o Responsable Inscripto? \n Si eres Consumidor Final escribe: A \n Si eres Responsable Inscripto escribe: B")).toUpperCase
+// let IVA = 1.21;
+// let IIBB = 1.04;
+
+// tipoConsumidor = function tipoContribucionTributaria(tipoConsumidor) {
+//     if (tipoConsumidor  === A) {
+//         return alert("Genial, eres Consumidor Final.\n Tú importe a pagar tendra una recarga del 21% correspondiente a IVA")
+//     }
+    
+//     if (tipoConsumidor === B){
+//         return alert("Genial, eres Responsable Inscripto.\n Tú importe a pagar tendra una recarga del 21% correspondiente a IVA y un 4% correspondiente a IIBB.\n No olvides que en tú cierre fiscal mensual ambos impuestos se contemplan.")
+//     }
+// }
+// tipoContribucionTributaria();
+
+// let importePagar = parseInt(prompt("¿Cuál es tú monto a pagar?"))
+
+// function recargoCuotas(tipoContribucionTributaria ,IVA, importePagar, cuotas) {
+//     if (tipoContribucionTributaria === A) {
+//         importePagar*IVA
+        
+//     }
+//     let cuotas = parseInt(prompt("¿En cuantas cuotas deseas pagar?"))
+// }
+// recargoCuotas();
